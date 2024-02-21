dd931a649a10dcc39e75d8d3ddfe430161bb4129
bhatthappy <bhatthappy02@gmail.com>
2024-02-21 10:07:43 +0530

    navbar created using mui

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,83 @@
+import { Grid, Link } from "@mui/material";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+export default function NavBar() {
+  return (
+    <>
+      <Grid container textAlign={"center"} rowSpacing={2}>
+        <Grid item xs={2}>
+          <Link
+            href="/Home"
+            style={{
+              textDecoration: "none",
+              color: "#000",
+            }}
+          >
+            Home
+          </Link>
+        </Grid>
+        <Grid item xs={2}>
+          <Link
+            href="/Home"
+            style={{
+              textDecoration: "none",
+              color: "#000",
+            }}
+          >
+            <Grid container>
+              <Grid item xs={6} textAlign={"end"}>
+                Shop
+              </Grid>
+              <Grid item xs={6}>
+                <KeyboardArrowDownIcon fontSize="small" />
+              </Grid>
+            </Grid>
+          </Link>
+        </Grid>
+        <Grid item xs={2}>
+          <Link
+            href="/Home"
+            style={{
+              textDecoration: "none",
+              color: "#000",
+            }}
+          >
+            Our Story
+          </Link>
+        </Grid>
+        <Grid item xs={2}>
+          <Link
+            href="/Home"
+            style={{
+              textDecoration: "none",
+              color: "#000",
+            }}
+          >
+            Blog
+          </Link>
+        </Grid>
+        <Grid item xs={2}>
+          <Link
+            href="/Home"
+            style={{
+              textDecoration: "none",
+              color: "#000",
+            }}
+          >
+            Contact Us
+          </Link>
+        </Grid>
+        <Grid item xs={2}>
+          <Link
+            href="/Home"
+            style={{
+              textDecoration: "none",
+              color: "#000",
+            }}
+          >
+            Help
+          </Link>
+        </Grid>
+      </Grid>
+    </>
+  );
+}
