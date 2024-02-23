4b268e7b9d84965770908ef693ad7bdd46a95d86
bhatthappy <bhatthappy02@gmail.com>
2024-02-23 23:30:54 +0530

    Home Page Update

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -1,17 +1,17 @@
-import { Grid } from "@mui/material";
-import krist_Logo from "../assets/Krist_logo.png";
+import { Grid, Typography } from "@mui/material";
+import krist_Logo from "../assets/Krist_logo_New.png";
+import CallIcon from "@mui/icons-material/Call";
 
 export default function Footer() {
   return (
     <>
       <Grid container mt={10} sx={{ backgroundColor: "#000" }}>
         <Grid item xs={3} textAlign={"center"}>
-          <img
-            src={krist_Logo}
-            alt="Krist Logo"
-            height={"35px"}
-            style={{ background: "#fff", padding: "10px" }}
-          />
+          <img src={krist_Logo} alt="Krist Logo" />
+          <Typography color={"#fff"} textAlign={"left"} ml={10} mt={2}>
+            <CallIcon />
+          </Typography>
+          <Typography variant="body2" color={""}
         </Grid>
         <Grid item xs={3}></Grid>
         <Grid item xs={3}></Grid>
